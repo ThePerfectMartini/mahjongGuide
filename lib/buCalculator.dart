aa9ab1edc14ce05b2680674cbf45030a8438a96d
vodka <rhd9228@gmail.com>
2024-06-19 23:45:25 +0900

    부수 계산 함수 todo 주석 추가

--- lib/buCalculator.dart
+++ lib/buCalculator.dart
@@ -1,4 +1,38 @@
 
-buCalculator(){
+buCalculator(Map hand_map){
+  int bu = 20;
+  bool menzen = false;
+
+  List<int> huro = hand_map['후로'];
+
+  // Todo: 멤버변수 리스트 
   
+  /**
+   * 부수
+   * 멘젠 여부
+   * 
+   * 
+   */
+
+  // Todo: 멘젠 상태 체크, 부수 추가
+
+
+  // Todo: 화료 타입별 부수 추가
+
+
+
+  // Todo: 몸통별 부수 추가
+
+
+
+  // Todo: 머리 특수 부수 추가
+
+
+
+  // Todo: 대기 부수 추가
+
+
+
+  // Todo: 리턴으로 부수 제공
+
 }
